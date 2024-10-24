cbca35f7b65536b8cc4d2fe4bcf49745e22ae934
KiirGr <workmailkirill@gmail.com>
2024-10-24 15:45:26 +0300

    remove adress from list, block submit empty value

--- src/App.vue
+++ src/App.vue
@@ -68,6 +68,11 @@ export default {
 
     // добавление адреса в список и отправка координат точки
     addPoint(adressInputValue) {
+      if (!adressInputValue) {
+        alert('Введите значение')
+        return
+      }
+
       this.adressField = adressInputValue
 
       const resultCoordinates = window.ymaps.geocode(this.adressField)
@@ -78,6 +83,7 @@ export default {
         })
         .catch(err => alert(`Произошла ошибка: ${err}`))
       this.adressList.push(this.adressField)
+      console.log(this.adressList)
       this.adressField = ''
     },
     buttonClick() {
@@ -125,8 +131,10 @@ export default {
 
     pointToDelete(value) {
       this.coordinatesArr.splice(value, 1)
+      this.adressList.splice(value, 1)
+      console.log(this.adressList)
       const points = JSON.parse(JSON.stringify(this.coordinatesArr))
-      console.log(this.coordinatesArr)
+      // console.log(this.coordinatesArr)
       const toRawMap = toRaw(this.myMap)
       toRawMap.geoObjects.removeAll()
 
